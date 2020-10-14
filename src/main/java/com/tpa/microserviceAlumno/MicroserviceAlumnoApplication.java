80d0ffe00856d6212361477ee1062887bfcf5dd0
nachogg18 <44012745+nachogg18@users.noreply.github.com>
2020-10-13 21:40:22 -0300

    Commit inicial

---
+++ src/main/java/com/tpa/microserviceAlumno/MicroserviceAlumnoApplication.java
@@ -0,0 +1,13 @@
+package com.tpa.microserviceAlumno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAlumnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAlumnoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpa/microserviceAlumno/MicroserviceAlumnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpa.microserviceAlumno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAlumnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
